61da1ee6ce258ab137de0335153eb4e6de27903b
asa <cyberasasoftware@gmail.com>
2024-03-21 14:35:28 -0500

    WIP register/login

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -3,12 +3,32 @@ const Register = () => {
 
 
   return (
-    <>
-      <div className="start-body">
-      < Navbar/>
-        register
+    <div>
+      <Navbar />
+      <div className="register-container">
+      
+        <div className="register-body">
+          <div className="register-text">Welcome to ESDIR </div>
+          <div className="register-upload-body">
+            <div className="divider"/>
+            <div className="register-upload-text"> Upload a New Building</div>
+            <div className="divider"/>
+
+            <div className="upload-items">
+              <label className="item-label">New Building</label>
+              <input className="item-input"/>
+            </div>
+            <div className="upload-items">
+              <label className="item-label">Coordinates</label>
+              <input className="item-input"/>
+            </div>
+            <div className="btn-container">
+              <button className="btn-cartpage register-button">Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
