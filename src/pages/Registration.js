64ae67170ebc0d970e540f398cef441d299d5464
asa <cyberasasoftware@gmail.com>
2024-03-22 22:57:37 -0500

    done

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,37 +1,15 @@
 import Navbar from "../components/Navbar";
+import building from "../images/building.png";
 const Register = () => {
-
-
   return (
-    <div>
+    <>
       <Navbar />
       <div className="register-container">
-      
-        <div className="register-body">
-          <div className="register-text">Welcome to ESDIR </div>
-          <div className="register-upload-body">
-            <div className="divider"/>
-            <div className="register-upload-text"> Upload a New Building</div>
-            <div className="divider"/>
-
-            <div className="upload-items">
-              <label className="item-label">New Building</label>
-              <input className="item-input"/>
-            </div>
-            <div className="upload-items">
-              <label className="item-label">Coordinates</label>
-              <input className="item-input"/>
-            </div>
-            <div className="btn-container">
-              <button className="btn-cartpage register-button">Submit</button>
-            </div>
-          </div>
-        </div>
+        <div className="register-title">Welcome to ESDIR </div>
+        <div className="register-body"></div>
       </div>
-    </div>
+    </>
   );
 };
 
 export default Register;
-
-
