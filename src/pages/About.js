798f28bc68d1570e68ac58c4b53ab10af61aebdf
Terriron Fields <112105884+TerrironFields256@users.noreply.github.com>
2024-03-27 21:14:08 -0500

    new

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,46 @@ import Navbar from "../components/Navbar";
 function About() {
     return (
       <div className="About_body">
-                < Navbar/>
-        About
+              
+        <div className="frame">
+          <div className="About-container">
+          < Navbar/>
+              <div className="overlap-group">
+                
+                
+                <h1 className="e-s-d-i-r">E&nbsp;&nbsp; S&nbsp;&nbsp; D&nbsp;&nbsp; I&nbsp;&nbsp; R</h1>
+                <h2 className="esdir-name">Electromechanical Self-Driven Intelligent Rover</h2>
+                <p className="text">
+                  <span className="span">
+                      <br />
+                      <br />
+                  </span>
+                  
+                  <span className="span">
+                      <br />
+                  </span>
+                </p>
+
+
+                <p className="ESDIR-description">
+                 ESDIR is a robot capable of efficiently maneuvering through a maze, avoiding obstacles and objects, while reaching its intended destination.
+                </p>
+
+                <p className="div">
+                  ESDIR where Service is Sovereignty!
+                </p>
+
+                <p className="p">
+                  ESDIR is controlled through the use of a web application with robust user authentication features, which enable users to submit delivery requests. These locations come from a pre-defined list of campus buildings, each linked to specific GPS locations. Admins will have the ability to add GPS locations to the list making this product more efficient for users.
+                </p>
+
+                
+
+              </div>
+          </div>
+        </div>
+
+
       </div>
       
     );
